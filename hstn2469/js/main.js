2749f35238c55b06f18b4bbf658f2e7a114c66d6
jordancz <tsyouasd@163.com>
2019-03-16 01:56:41 +0800

    update

--- hstn2469/js/main.js
+++ hstn2469/js/main.js
@@ -4,23 +4,24 @@ $(document).ready(function() {
     // If already in blog, return early without animate overlay panel again.
     if (location.hash && location.hash == "#blog") return;
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
-    $('.main-post-list').removeClass('hidden');
+    $('.main-post-list').removeClass('hidden'); 
     currentWidth = $('.panel-cover').width();
     if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed');
+      $('.panel-cover').addClass('panel-cover--collapsed');       
     } else {
       $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '400px', 'width': '30%'}, 400, swing = 'swing', function() {} );
+      $('.panel-cover').animate({'max-width': '400px', 'width': '30%'}, 400, swing = 'swing', function() {} );       
     }
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
     $('.panel-cover').addClass('panel-cover--collapsed');
-    $('.main-post-list').removeClass('hidden');
+    $('.main-post-list').removeClass('hidden'); 
   }
 
   if (window.location.pathname.substring(0, 5) == "/tag/") {
     $('.panel-cover').addClass('panel-cover--collapsed');
+
   }
 
   $('.btn-mobile-menu__icon').click(function() {
